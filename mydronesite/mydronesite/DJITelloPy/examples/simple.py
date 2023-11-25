f9ba721cc03d9d67e3d3eb28913a24a715a1f239
RogueBucket <52482721+mickyreed@users.noreply.github.com>
2023-11-25 16:05:34 +0800

    reinstalled DJITelloPY now working

---
+++ mydronesite/mydronesite/DJITelloPy/examples/simple.py
@@ -0,0 +1,13 @@
+from djitellopy import Tello
+
+tello = Tello("127.0.0.1")
+
+
+tello.connect()
+tello.takeoff()
+
+tello.move_left(100)
+tello.rotate_clockwise(90)
+tello.move_forward(100)
+
+tello.land()
